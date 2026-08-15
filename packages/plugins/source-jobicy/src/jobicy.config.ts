7c84baeb3dfd180c9fc9eddf3937e0f8c1dfc4c7
Jim P <moomf@hotmail.com>
2026-08-15 03:35:09 +0100

    fix(jobicy): add location slug config

---
+++ packages/plugins/source-jobicy/src/jobicy.config.ts
@@ -0,0 +1,47 @@
+export const JOBICY_LOCATION_SLUGS: Record<string, string> = {
+  // UK & Ireland
+  'united kingdom': 'uk',
+  uk: 'uk',
+  'great britain': 'uk',
+  ireland: 'ireland',
+
+  // Western / Northern Europe
+  france: 'france',
+  germany: 'germany',
+  netherlands: 'netherlands',
+  belgium: 'belgium',
+  spain: 'spain',
+  portugal: 'portugal',
+  italy: 'italy',
+  switzerland: 'switzerland',
+  austria: 'austria',
+  denmark: 'denmark',
+  sweden: 'sweden',
+  norway: 'norway',
+  finland: 'finland',
+
+  // North America
+  'united states': 'usa',
+  'united states of america': 'usa',
+  usa: 'usa',
+  us: 'usa',
+  canada: 'canada',
+
+  // Australia / New Zealand
+  australia: 'australia',
+  'new zealand': 'new-zealand',
+  'new-zealand': 'new-zealand',
+
+  // Asia
+  singapore: 'singapore',
+  'hong kong': 'hong-kong',
+  'hong-kong': 'hong-kong',
+  japan: 'japan',
+  'south korea': 'south-korea',
+  'south-korea': 'south-korea',
+
+  // Useful regional searches
+  europe: 'europe',
+  emea: 'emea',
+  apac: 'apac',
+};
